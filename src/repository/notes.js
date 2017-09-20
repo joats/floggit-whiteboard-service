b1363cd1d6ee9b5973fc904b7d1d19f2ebd35e0b
johsjo <johsjo83@gmail.com>
2017-09-20 15:34:51 +0200

    1 merge project (#1)
    * updated gitignore
    
    * added dependencies packages
    
    * added helpers
    
    * added controllers & middleware & routes
    
    * added services
    
    * added repository

---
+++ src/repository/notes.js
@@ -0,0 +1,48 @@
+// NOTE we use localStorage in this example to simplify/mock a backend.
+// Also we use to illustrate how frontend code can be used on the backend.
+// In production you should use a proper data store i.e. mySql, redis, couchdb etc.
+const { LocalStorage } = require('node-localstorage');
+
+const localStorage = new LocalStorage('./scratch');
+
+const notes = (localStorage.getItem('noteList')) ?
+  JSON.parse(localStorage.getItem('noteList')) : {};
+
+function generateId() {
+  return (+(new Date())).toString(); // Use a GUID generator instead of this
+}
+
+function save() {
+  localStorage.setItem('noteList', JSON.stringify(notes));
+}
+
+const publicAPI = {};
+
+publicAPI.add = (value) => {
+  const uniqueId = generateId();
+  notes[uniqueId] = {
+    value,
+  };
+  save();
+  return uniqueId;
+};
+
+publicAPI.get = id => notes[id];
+
+publicAPI.remove = (id) => {
+  delete notes[id];
+  save();
+};
+
+publicAPI.getAll = () => {
+  const notesArray = [];
+  Object.keys(notes).forEach((id) => {
+    notesArray.push({
+      id,
+      value: notes[id].value,
+    });
+  });
+  return notesArray;
+};
+
+module.exports = publicAPI;
---
+++ src/service/v1/controllers/notes.js
@@ -0,0 +1,39 @@
+const notes = require('../../../repository/notes');
+
+const get = (req, res) => {
+  const id = req.params.id;
+  const note = notes.get(id);
+  if (note) {
+    res.json(notes.get(id));
+  } else {
+    res.status(404).json({
+      type: 'error',
+      message: `Note ${id} not found` });
+  }
+};
+
+const getAll = (req, res) => {
+  res.json(notes.getAll());
+};
+
+const remove = (req, res) => {
+  const id = req.params.id;
+  notes.remove(id);
+  res.status(204).send();
+};
+
+const add = (req, res) => {
+  const note = req.body.value;
+  const id = notes.add(note);
+  res.status(201).json({ id });
+};
+
+// TODO create update/put
+const update = (req, res) => {};
+
+module.exports = {
+  get,
+  getAll,
+  remove,
+  add,
+};
