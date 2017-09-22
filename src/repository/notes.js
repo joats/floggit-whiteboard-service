f8441f7ea3675316b94728d034b883091518c73e
Emelie  Mirhagen <emelie.mirhagen@gmail.com>
2017-09-22 11:52:28 +0200

    finished guid

--- src/repository/notes.js
+++ src/repository/notes.js
@@ -2,16 +2,12 @@
 // Also we use to illustrate how frontend code can be used on the backend.
 // In production you should use a proper data store i.e. mySql, redis, couchdb etc.
 const { LocalStorage } = require('node-localstorage');
+const uuid = require('uuid/v4');
 
 const localStorage = new LocalStorage('./scratch');
 
 const notes = (localStorage.getItem('noteList')) ?
   JSON.parse(localStorage.getItem('noteList')) : {};
-
-//function generateId(a) {
-  //return a?(a^Math.random()*16>>a/4).toString(16):([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g,generateId);
-//}
-const generateId = Guid.create();
 
 function save() {
   localStorage.setItem('noteList', JSON.stringify(notes));
@@ -20,12 +16,12 @@ function save() {
 const publicAPI = {};
 
 publicAPI.add = (value) => {
-  const uniqueId = generateId.value;
-  notes[uniqueId] = {
+  const guid = uuid();
+  notes[guid] = {
     value,
   };
   save();
-  return uniqueId;
+  return guid;
 };
 
 publicAPI.get = id => notes[id];
